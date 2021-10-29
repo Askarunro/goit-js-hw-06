db241103a69d1170b886ef6996d973275e9c73eb
Askarunro <paskar.y.a@gmail.com>
2021-10-29 23:17:53 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// const ingredientEl = document.createElement('li');
+const makeItemIngredient = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.textContent = ingredient;
+    console.log(ingredientEl);
+    ingredientEl.classList.add('item');
+    return ingredientEl;
+  });
+};
+const elements = makeItemIngredient(ingredients);
+const listUl = document.querySelector('#ingredients');
+
+listUl.append(...elements);
