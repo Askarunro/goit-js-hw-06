af9c300251575e00952b026e356ffe14d5025eed
Askarunro <paskar.y.a@gmail.com>
2021-10-29 22:59:02 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const ulEl = document.querySelector('#categories');
+const totalUlEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${totalUlEl.length}`);
+
+const a = totalUlEl.forEach(element => {
+  console.log(
+    `Category: ${element.querySelector('h2').textContent}, Elements: ${
+      element.querySelectorAll('li').length
+    }`,
+  );
+});
