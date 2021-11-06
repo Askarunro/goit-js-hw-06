5d1766f09010ff4a7dd36e55af2fe07f381890a5
Askarunro <paskar.y.a@gmail.com>
2021-11-06 19:43:22 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+
+button.addEventListener('click', even => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+});
