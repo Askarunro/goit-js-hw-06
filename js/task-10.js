1c247c174cd1613167bf6e5c2ba44e38f1739278
Askarunro <paskar.y.a@gmail.com>
2021-11-06 20:07:29 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const input = document.querySelector('#controls input');
+const buttonCreate = document.querySelector('#controls button[data-create]');
+const buttonDestroy = document.querySelector('#controls button[data-destroy]');
+
+input.addEventListener('change', even => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  buttonCreate.addEventListener('change', even => {});
+  buttonDestroy.addEventListener('change', even => {});
+});
