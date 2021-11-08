375ad9b73d4b3f9afcbe68626e2208b87b9f0000
Askarunro <paskar.y.a@gmail.com>
2021-11-08 19:58:30 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,9 @@ input.addEventListener('blur', even => {
   const minLength = +input.dataset.length;
   if (even.currentTarget.value.length >= minLength) {
     input.classList.add('valid');
+    input.classList.remove('invalid');
   } else {
+    input.classList.remove('valid');
     input.classList.add('invalid');
   }
 });
