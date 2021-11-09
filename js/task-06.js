f67503c7369d89ec6f3a67b93f0f08c4fbcdf761
Askarunro <paskar.y.a@gmail.com>
2021-11-09 20:54:08 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', even => {
   const minLength = +input.dataset.length;
-  if (even.currentTarget.value.length >= minLength) {
+  if (even.currentTarget.value.length === minLength) {
     input.classList.add('valid');
     input.classList.remove('invalid');
   } else {
